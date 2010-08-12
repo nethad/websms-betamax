0041d6adbbf36dc6ea962de504810056583ae6af
Felix Bechstein <f@ub0r.de>
2010-08-12 22:25:13 +0200

    let WebSMSException extend RuntimeException

--- src/de/ub0r/android/websms/connector/betamax/ConnectorBetamax.java
+++ src/de/ub0r/android/websms/connector/betamax/ConnectorBetamax.java
@@ -23,14 +23,19 @@ import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URLEncoder;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
 import org.apache.http.HttpResponse;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.util.Log;
-
 import de.ub0r.android.websms.connector.common.Connector;
 import de.ub0r.android.websms.connector.common.ConnectorCommand;
 import de.ub0r.android.websms.connector.common.ConnectorSpec;
@@ -38,22 +43,13 @@ import de.ub0r.android.websms.connector.common.Utils;
 import de.ub0r.android.websms.connector.common.WebSMSException;
 import de.ub0r.android.websms.connector.common.ConnectorSpec.SubConnectorSpec;
 
-
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.ParserConfigurationException;
-
-import org.w3c.dom.Document;
-import org.xml.sax.SAXException;
-
-
 /**
  * AsyncTask to manage IO to betamax API.
  * 
  * @author flx
  */
 public class ConnectorBetamax extends Connector {
-	
+
 	/** Tag for debug output. */
 	private static final String TAG = "WebSMS.betamax";
 	/** SmsBug Gateway URL. */
@@ -64,7 +60,6 @@ public class ConnectorBetamax extends Connector {
 	/**
 	 * {@inheritDoc}
 	 */
-	@SuppressWarnings("deprecation")
 	@Override
 	public final ConnectorSpec initSpec(final Context context) {
 		final String name = context.getString(R.string.connector_betamax_name);
@@ -74,7 +69,7 @@ public class ConnectorBetamax extends Connector {
 		c.setCapabilities(ConnectorSpec.CAPABILITIES_UPDATE
 				| ConnectorSpec.CAPABILITIES_SEND
 				| ConnectorSpec.CAPABILITIES_PREFS);
-		//c.setValidCharacters(CharacterTable.getValidCharacters());
+		// c.setValidCharacters(CharacterTable.getValidCharacters());
 		c.addSubConnector(TAG, name, SubConnectorSpec.FEATURE_MULTIRECIPIENTS);
 		return c;
 	}
@@ -107,11 +102,8 @@ public class ConnectorBetamax extends Connector {
 	 * @param ret
 	 *            return code
 	 * @return true if no error code
-	 * @throws WebSMSException
-	 *             WebSMSException
 	 */
-	private boolean checkReturnCode(final Context context, final int ret)
-			throws WebSMSException {
+	private boolean checkReturnCode(final Context context, final int ret) {
 		Log.d(TAG, "ret=" + ret);
 		if (ret < 200) {
 			return true;
@@ -133,11 +125,8 @@ public class ConnectorBetamax extends Connector {
 	 *            {@link Context}
 	 * @param command
 	 *            {@link ConnectorCommand}
-	 * @throws WebSMSException
-	 *             WebSMSException
 	 */
-	private void sendData(final Context context, final ConnectorCommand command)
-			throws WebSMSException {
+	private void sendData(final Context context, final ConnectorCommand command) {
 		// do IO
 		try { // get Connection
 			final String text = command.getText();
@@ -157,34 +146,37 @@ public class ConnectorBetamax extends Connector {
 			}
 			url.append("?from=");
 
-			url.append(URLEncoder.encode(Utils.getSender(context, command.getDefSender()))
-					.replace("+", ""));
+			url.append(URLEncoder.encode(
+					Utils.getSender(context, command.getDefSender())).replace(
+					"+", ""));
 			url.append("&username=");
-			url.append(URLEncoder.encode(p.getString(Preferences.PREFS_USER, "")));
+			url.append(URLEncoder.encode(p
+					.getString(Preferences.PREFS_USER, "")));
 			url.append("&password=");
-			url.append(URLEncoder.encode(p.getString(Preferences.PREFS_PASSWORD, "")));
+			url.append(URLEncoder.encode(p.getString(
+					Preferences.PREFS_PASSWORD, "")));
 
 			if (!checkOnly) {
 				url.append("&text=");
 				url.append(URLEncoder.encode(text));
 				url.append("&to=");
-				url.append(URLEncoder.encode(Utils.national2international(command.getDefPrefix(),
+				url.append(URLEncoder.encode(Utils.national2international(
+						command.getDefPrefix(),
 						Utils.getRecipientsNumber(command.getRecipients()[0]))
 						.substring(1)));
 
 			}
-			
+
 			// send data
 			HttpResponse response = Utils.getHttpClient(url.toString(), null,
-					null, null, null);
+					null, null, null, false);
 			int resp = response.getStatusLine().getStatusCode();
 			if (resp != HttpURLConnection.HTTP_OK) {
 				this.checkReturnCode(context, resp);
 				throw new WebSMSException(context, R.string.error_http, " "
 						+ resp);
 			}
-			
-			
+
 			if (checkOnly) {
 				InputStream htmlStream = response.getEntity().getContent();
 				String htmlText = Utils.stream2str(htmlStream).trim();
@@ -193,23 +185,30 @@ public class ConnectorBetamax extends Connector {
 				for (String s : lines) {
 					cs.setBalance(s.replace("| &#8364;", "\u20AC"));
 				}
-		    
+
 			} else {
 				// Parse XML response looking for resultstring value
-				DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-			    DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-			    Document doc = dBuilder.parse(response.getEntity().getContent());
-			    doc.getDocumentElement().normalize();
-			    Integer nValue = Integer.parseInt(doc.getElementsByTagName("result").item(0).getChildNodes().item(0).getNodeValue());
-			    String nValueString = doc.getElementsByTagName("resultstring").item(0).getChildNodes().item(0).getNodeValue();
+				DocumentBuilderFactory dbFactory = DocumentBuilderFactory
+						.newInstance();
+				DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+				Document doc = dBuilder
+						.parse(response.getEntity().getContent());
+				doc.getDocumentElement().normalize();
+				Integer nValue = Integer.parseInt(doc.getElementsByTagName(
+						"result").item(0).getChildNodes().item(0)
+						.getNodeValue());
+				String nValueString = doc.getElementsByTagName("resultstring")
+						.item(0).getChildNodes().item(0).getNodeValue();
 
 				// Use WebSMSException for failure messages
 				if (nValue < 1) {
-					Log.d(TAG, "failed to send message via Betamax vendor, response following:");
+					Log
+							.d(TAG,
+									"failed to send message via Betamax vendor, response following:");
 					Log.d(TAG, nValueString);
-					//Log.d(TAG, "Request following:");
-					//Log.d(TAG, url.toString());
-					
+					// Log.d(TAG, "Request following:");
+					// Log.d(TAG, url.toString());
+
 					throw new WebSMSException(context, R.string.error_sending);
 				}
 			}
@@ -228,8 +227,7 @@ public class ConnectorBetamax extends Connector {
 	 * {@inheritDoc}
 	 */
 	@Override
-	protected final void doUpdate(final Context context, final Intent intent)
-			throws WebSMSException {
+	protected final void doUpdate(final Context context, final Intent intent) {
 		this.sendData(context, new ConnectorCommand(intent));
 	}
 
@@ -237,8 +235,7 @@ public class ConnectorBetamax extends Connector {
 	 * {@inheritDoc}
 	 */
 	@Override
-	protected final void doSend(final Context context, final Intent intent)
-			throws WebSMSException {
+	protected final void doSend(final Context context, final Intent intent) {
 		this.sendData(context, new ConnectorCommand(intent));
-	}		 
+	}
 }
